6ee60ba4aba4f4633a0e76731100bb16d384a507
pavankalyan0105 <pavankalyan0105@gmail.com>
2021-05-26 14:52:49 +0530

    values are inserting into the DB

--- projback/index.js
+++ projback/index.js
@@ -15,7 +15,7 @@ var db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "Pavan@123",
-    database:"library" , 
+    database: "library", 
     insecureAuth : true
 });
 
@@ -26,16 +26,19 @@ db.connect( (err) =>{
 
 
 app.post('/create', (req, res)=>{
-    const regdno = req.body.regdno;
-    const name   = req.body.name;
-    const branch = req.body.branch;
-    const email  = req.body.email;
-    const phoneno = req.body.phoneno;
+    const regdno1 = req.body.regdno;
+    const name1   = req.body.name;
+    const branch1 = req.body.branch;
+    const email1  = req.body.email;
+    const phoneno1 = req.body.phoneno;
     
-    console.log(regdno , name , branch , email , phoneno);
+    console.log(regdno1 , name1 , branch1 , email1 , phoneno1);
+
+    let query = `INSERT INTO students (name, email) VALUES (?, ?);`;
+    db.query(query , [name1, email1] ,
 
     // db.query('INSERT INTO students(regdno , name, branch , phoneno ,email) VALUES(regdno, name , branch , phoneno , email)' , 
-    db.query('INSERT INTO students(regdno) VALUES("1223")', 
+//    db.query('INSERT INTO `students` (regdno1 , name1 , branch1 , phoneno1 , email1) VALUES(regdno, name , branch , phoneno , email)', 
 
     (err, res) =>{
         
